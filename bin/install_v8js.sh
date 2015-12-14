697ac81ee639ddecf6be13deea3123180aae71d9
Boris Strochkov <strochkov@gmail.com>
2015-12-14 15:42:17 +0000

    Пытаюсь установить v8js

--- bin/install_v8js.sh
+++ bin/install_v8js.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-# Build Path: /app/.heroku/php/
+#!/usr/bin/env bash
+# bin/compile <build-dir> <cache-dir>
 
 git clone https://github.com/FriendsOfPHP/pickle.git
 cd pickle
 chmod +x bin/pickle
-apt-get install -y libv8js-dev
 bin/pickle install v8js-0.1.3
 echo "extension=v8js.so" >> /app/.heroku/php/etc/php/php.ini
